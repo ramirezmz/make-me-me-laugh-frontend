b8aa8daad60af2abc1693c36137488baecfa1ea6
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-09-18 21:39:21 -0300

    feat: create poker face view page

---
+++ src/views/PokerFaceView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1>Poker Face</h1>
+    <h2>Piada</h2>
+    <span>
+      em Ipsum has, but also the leap into electronic typesetting, remaining
+      essentially unchanged. It was popularised in the 1960s with the release of
+      Letraset s</span
+    >
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped>
+.container {
+  background-color: var(--bg-c-poker-face);
+  height: 100vh;
+}
+</style>
