dac373fecfb1ea4dd5860d84e4ef9571a2608d12
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-09-18 21:29:47 -0300

    feat: create Happy view page

---
+++ src/views/HappyView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <h1>Feliz</h1>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped>
+.container {
+  background-color: var(--bg-c-happy);
+  height: 100vh;
+}
+</style>
