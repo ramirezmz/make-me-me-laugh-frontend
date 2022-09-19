3226e68b25ea180ef652fbae702df402279b93c1
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-09-18 21:45:04 -0300

    feat: create sad view page

---
+++ src/views/SadView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <h1>Triste</h1>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped>
+.container {
+  background-color: var(--bg-c-sad);
+  height: 100vh;
+  color: #fff;
+}
+</style>
