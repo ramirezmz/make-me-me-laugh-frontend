00a92b515d6b19429d50de629aac62ced00e93f9
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-09-18 21:29:23 -0300

    feat: create login page

---
+++ src/views/LoginView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <img src="@/assets/space-bg-2.jpg" alt="space" class="img__background" />
+    <Form />
+  </div>
+</template>
+<script lang="ts" setup>
+import Form from "@/components/LoginForm/Form.vue";
+</script>
+<style scoped>
+img {
+  width: 60vw;
+  height: 99vh;
+  position: relative;
+}
+
+@media (max-width: 1056px) {
+  img {
+    width: 0vw;
+    height: 0vh;
+  }
+}
+</style>
