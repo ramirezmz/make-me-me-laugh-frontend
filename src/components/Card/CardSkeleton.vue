2934f835803ca4de9060074c9ebb83dbc451b391
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-09-19 20:55:57 -0300

    feat: add skeleton card

---
+++ src/components/Card/CardSkeleton.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="profile-card">
+    <div class="profile-image">
+      <img />
+    </div>
+    <div class="profile-info">
+      <span />
+      <h3 />
+      <p />
+    </div>
+  </div>
+</template>
+<style scoped>
+.profile-image img {
+  width: 100%;
+  padding-top: 100%;
+  background-color: #eee;
+}
+
+.profile-info span {
+  width: 100%;
+  height: 16px;
+  display: block;
+  background-color: #eee;
+}
+
+.profile-info h3 {
+  width: 250px;
+  height: 24px;
+  background-color: #eee;
+}
+
+.profile-info p {
+  width: 80%;
+  height: 16px;
+  background-color: #eee;
+}
+</style>
