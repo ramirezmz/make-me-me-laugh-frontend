7ced373d369251710dbf34ee60eda93ed1bb2e78
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-09-19 20:57:38 -0300

    feat: add info to interactive user

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,15 @@
 <template>
   <RouterLink to="/sad">
     <div class="container">
-      <h1>Nem feliz nem triste</h1>
+      <h1>Welcome to sadic test!</h1>
+      <span
+        >This face below is Pepito, today, Pepito needs to help to have a good
+        day, perphaps a joke resolve all</span
+      >
       <img
         src="https://img.icons8.com/emoji/96/000000/expressionless-face.png"
       />
+      <span>(Click to continue)</span>
     </div>
   </RouterLink>
 </template>
@@ -13,9 +18,12 @@ import { RouterLink } from "vue-router";
 </script>
 <style scoped>
 .container {
-  background-color: var(--bg-c-neutro);
+  background: #403b4a;
+  background: -webkit-linear-gradient(to right, #e7e9bb, var(--bg-c-neutro));
+  background: linear-gradient(to right, #e7e9bb, var(--bg-c-neutro));
   height: 100vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 }
